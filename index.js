3226b7187a5a12d68d4d05ec902dd972961a422c
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-08-31 17:06:43 -0300

    --validate corregido

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 #!/usr/bin/env node
 
-//const mdLinks = require('./lib/md-links').mdLinks;
-
-
 const [, , ...args] = process.argv;
 
 let fileName = args[0];
 
 let validate = args[1];
-
-
-
 
 const mdlinks = require('./lib/md-links').mdLinks(fileName, validate);
 
