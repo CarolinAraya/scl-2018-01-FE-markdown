e64514fc9677ece4b748d5fc03ef269ad8666d66
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-08-31 16:09:58 -0300

    sólo ingresando y validando archivo

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
 
-const mdLinks = require('./lib/md-links').mdLinks;
+//const mdLinks = require('./lib/md-links').mdLinks;
 
-if (require.main === module) {
+
   const [, , ...args] = process.argv;
 
   let fileName = args[0];
 
-  let options = {
+/*   let options = {
     validate = args[1].includes('--validate'),
-  };
+  }; */
 
  // options.validate = args[1];
 
 
-  mdlinks(fileName, option);
+ const mdlinks = require('./lib/md-links').mdLinks(fileName, null);
 
-}
+
 
 
 
