e28709f7e24f312bf86bf3c3f7df59692c36b706
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-08-31 15:55:34 -0300

    ingresando los argumentos

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const mdLinks = require('./lib/md-links').mdLinks;
 
 if (require.main === module) {
   const [, , ...args] = process.argv;
-  
+
+  let fileName = args[0];
+
   let options = {
-	  validate = args.includes('--validate'),
+    validate = args[1].includes('--validate'),
   };
 
- 
+ // options.validate = args[1];
+
+
+  mdlinks(fileName, option);
+
 }
+
 
 
 
