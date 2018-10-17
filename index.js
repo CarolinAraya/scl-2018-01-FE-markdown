a19e68d7aef752ea1bada434c5b5f00a5d17545a
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-10-17 13:06:36 -0300

    devolviendo los resultados en la promesa de index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
 'use strict';
+let mdLinks = require('./lib/md-links');
+
+if (require.main === module) {
+    const [, , ...args] = process.argv;
+    let fileName = args[0];
+    let validate = args[1];
+
+    mdLinks(fileName, validate).then((result) => {
+        
+        if (result){
+            console.log("pez koi")
+        }
+        //return result;
+    })
+}
 
 module.exports = require('./lib/md-links');
-
-const [, , ...args] = process.argv;
-
-let fileName = args[0];
-
-let validate = args[1];
-
-const mdlinks = require('./lib/md-links').mdLinks(fileName, validate);
